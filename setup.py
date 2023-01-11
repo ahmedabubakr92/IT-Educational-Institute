ea0418ea47aef9ebb34332461a658fddd5c6e66c
Ahmed Ahmed <ahmedahmed@Ahmeds-MacBook-Pro-2.local>
2023-01-11 16:52:44 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A project for an IT Educational Institute. T. The objective is to develop a data-driven solution which helps the students know which skills they need to learn for exactly which job',
+    author='Ahmed',
+    license='',
+)
